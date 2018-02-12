170d699b9f8ec6e6fbf68bb4795e25364bbca0b2
Modestas <modestaso@gmail.com>
2018-02-12 20:36:53 +0200

    update dependencies

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import JssProvider from 'react-jss/lib/JssProvider';
-import { withStyles, MuiThemeProvider } from 'material-ui/styles';
-import createContext from 'styles/createContext';
+import { MuiThemeProvider } from 'material-ui/styles';
+import theme from 'styles/theme';
 import Routes from 'routes';
+import Reboot from 'material-ui/Reboot';
 import HeaderBar from 'components/HeaderBar';
 
-const styles = theme => ({
-  '@global': {
-    html: {
-      background: theme.palette.background.default,
-        WebkitFontSmoothing: 'antialiased', // Antialiasing.
-        MozOsxFontSmoothing: 'grayscale', // Antialiasing.
-    },
-    body: {
-      margin: 0,
-    },
-  },
-});
-
-const context = createContext();
 const App = ({ store }) =>
   <Provider store={store}>
-    <JssProvider registry={context.sheetsRegistry} jss={context.jss}>
-      <MuiThemeProvider theme={context.theme} sheetsManager={context.sheetsManager}>
-        <HeaderBar/>
-        <Routes/>
-      </MuiThemeProvider>
-    </JssProvider>
+    <MuiThemeProvider theme={theme}>
+      <Reboot/>
+      <HeaderBar/>
+      <Routes/>
+    </MuiThemeProvider>
   </Provider>;
 
-export default withStyles(styles)(App);
+export default App;
